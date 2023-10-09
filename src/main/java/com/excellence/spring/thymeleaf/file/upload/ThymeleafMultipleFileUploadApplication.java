41ef3d4e5e755f7bb28305fea92dff2996c991a7
Vijay Singh <vijaysinghaashu@gmail.com>
2023-10-09 10:52:23 +0530

    create-model-service-controller

---
+++ src/main/java/com/excellence/spring/thymeleaf/file/upload/ThymeleafMultipleFileUploadApplication.java
@@ -0,0 +1,21 @@
+package com.excellence.spring.thymeleaf.file.upload;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafMultipleFileUploadApplication implements CommandLineRunner {
+
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThymeleafMultipleFileUploadApplication.class, args);
+  }
+
+@Override
+public void run(String... args) throws Exception {
+	// TODO Auto-generated method stub
+	
+}
+
+}
